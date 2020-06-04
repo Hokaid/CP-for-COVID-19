b5cd714a94e24a21aad2569c06eb84c04358e8c8
Geral Esteen Castillo Arredondo <39970728+Hokaid@users.noreply.github.com>
2020-06-03 22:55:32 -0500

    visual

--- code/data_visualize.py
+++ code/data_visualize.py
@@ -5,71 +5,104 @@ import matplotlib.pyplot as plt # Data visualization
 plt.rcParams["figure.figsize"] = (40,15)
 from itertools import cycle
 import webbrowser
+from sklearn.model_selection import train_test_split
 
-n_hospitales = 10 #10 hospitales
-n_camas_en_hospitales = [2,18,7,10,2,2,10,6,7,10] #numero de camas
-n_camas_total = sum(n_camas_en_hospitales)
-
-def cargarCSV(csv, la, lo):
+def cargarCSV(csv):
     latitude_list = []
     longitude_list = []
-    cordenadas = []
+    distrito = []
     with open(csv, 'r') as read_obj:
-        # pass the file object to reader() to get the reader object
         csv_reader = reader(read_obj)
-        # Pass reader object to list() to get a list of lists
         list_of_rows = list(csv_reader)
-        #print(list_of_rows)
     for item in list_of_rows:
-        latitude_list.append(float(item[la]))
-        longitude_list.append(float(item[lo]))
-    cordenadas.append(latitude_list)
-    cordenadas.append(longitude_list)
-    return cordenadas
+        latitude_list.append(float(item[1]))
+        longitude_list.append(float(item[3]))
+    distrito.append(latitude_list)
+    distrito.append(longitude_list)
+    return distrito
 
-miraflores = cargarCSV('..\csv files\Miraflores.csv', 1, 3)
-sanIsidro = cargarCSV('..\csv files\SanIsidro.csv', 1, 3)
-surquillo = cargarCSV('..\csv files\Surquillo.csv', 1, 3)
-magdalena = cargarCSV('..\csv files\Magdalena.csv', 1, 3)
-hospitales = cargarCSV('..\csv files\Hospitales+Camas.txt', 2, 3)
+def cargarCSVHospitales(csv):
+    latitude_list = []
+    longitude_list = []
+    numeroCamas = []
+    uci = []
+    hospital = []
+    with open(csv, 'r') as read_obj:
+        csv_reader = reader(read_obj)
+        list_of_rows = list(csv_reader)
+    for item in list_of_rows:
+        latitude_list.append(float(item[2]))
+        longitude_list.append(float(item[3]))
+        numeroCamas.append(int(item[5]))
+        uci.append(int(item[4]))
+    hospital.append(latitude_list)
+    hospital.append(longitude_list)
+    hospital.append(numeroCamas)
+    hospital.append(uci)
+    return hospital
 
-#Localizacion Pacientes
-n_pacientes = len(miraflores[0])+len(sanIsidro[0])+len(surquillo[0])+len(magdalena[0])
-cantpaci = 0
-pacientes_loc = [(0, 0) for _ in range(n_pacientes)]
-for i in range(len(miraflores[0])):
-    pacientes_loc[i] = (miraflores[1][i], miraflores[0][i])
-cantpaci = len(miraflores[0])
-for i in range(cantpaci, cantpaci+len(sanIsidro[0])):
-    pacientes_loc[i] = (sanIsidro[1][i-cantpaci], sanIsidro[0][i-cantpaci])
-cantpaci = cantpaci+len(sanIsidro[0])
-for i in range(cantpaci, cantpaci+len(surquillo[0])):
-    pacientes_loc[i] = (surquillo[1][i-cantpaci], surquillo[0][i-cantpaci])
-cantpaci = cantpaci+len(surquillo[0])
-for i in range(cantpaci, n_pacientes):
-    pacientes_loc[i] = (magdalena[1][i-cantpaci], magdalena[0][i-cantpaci])
+miraflores = cargarCSV('..\csv files\Miraflores.csv')
+sanIsidro = cargarCSV('..\csv files\SanIsidro.csv')
+surquillo = cargarCSV('..\csv files\Surquillo.csv')
+magdalena = cargarCSV('..\csv files\Magdalena.csv')
+hospitales = cargarCSVHospitales('..\csv files\Hospitales+Camas.txt')
+p = []
+p.append(miraflores)
+p.append(sanIsidro)
+p.append(surquillo)
+p.append(magdalena)
 
-#Localizacion Hospitales
-hospitales_loc = [(0, 0) for _ in range(n_hospitales)]
+n_pacientes = 0
+n_hospitales = 0
+pacientes_loc = []
+hospitales_loc = []
+n_camas_en_hospitales = []
+pacientes_contagio = []
+
+pacientes = [] #Localizacion Pacientes
+for distrito in p:
+    for i in range(len(distrito[0])):
+        pacientes.append((distrito[0][i]*100, distrito[1][i]*100))
+h = [] #Localizacion Hospitales
 for i in range(len(hospitales[0])):
-    hospitales_loc[i] = (hospitales[1][i], hospitales[0][i])
+    h.append((hospitales[0][i]*100, hospitales[1][i]*100))
+n_hospitales = len(h) #Numero de hospitales
+hospitales_loc = h
 
-#grado de contagio
-pacientes_contagio = [0 for _ in range(n_pacientes)]
-for i in range(n_pacientes):
-    probgrado = random.random()
-    if probgrado <= 0.202: #Asymptomatic 20.2%
-        pacientes_contagio[i] = 1
-    elif probgrado > 0.202 and probgrado <= 0.549: #Mild 34.7%
-        pacientes_contagio[i] = 2
-    elif probgrado > 0.549 and probgrado <= 0.801: #Strong 25.2%
-        pacientes_contagio[i] = 3
-    elif probgrado > 0.801 and probgrado <= 0.939: #Hospitalization 13.8%
-        pacientes_contagio[i] = 4
-    elif probgrado > 0.939: #UCI 6.1%
-        pacientes_contagio[i] = 5
+def generar_datos(case):
+    camas = [] #Camas por hospital
+    for i in range(len(hospitales[2])):
+        if case == 0:
+            camas.append(hospitales[3][i])
+        elif case == 1:
+            camas.append(hospitales[2][i] + hospitales[3][i])
+    if case == 0:
+        hospitalizar = pacientes
+    elif case == 1:
+        paciente ,hospitalizar = train_test_split(pacientes,test_size=0.1)    
+    n_pacientes = len(hospitalizar) #Numero de pacientes
+    n_camas_en_hospitales = camas
+    n_camas_total = sum(n_camas_en_hospitales) #Numero de camas en total
+    # Localizacion
+    pacientes_loc = hospitalizar
+    print(n_pacientes, n_camas_total)
+    #grado de contagio
+    pacientes_contagio = [0 for _ in range(n_pacientes)]
+    for i in range(n_pacientes):
+        probgrado = random.random()
+        if probgrado <= 0.202: #Asymptomatic 20.2%
+            pacientes_contagio[i] = 1
+        elif probgrado > 0.202 and probgrado <= 0.549: #Mild 34.7%
+            pacientes_contagio[i] = 2
+        elif probgrado > 0.549 and probgrado <= 0.801: #Strong 25.2%
+            pacientes_contagio[i] = 3
+        elif probgrado > 0.801 and probgrado <= 0.939: #Hospitalization 13.8%
+            pacientes_contagio[i] = 4
+        elif probgrado > 0.939: #UCI 6.1%
+            pacientes_contagio[i] = 5
+    return n_camas_en_hospitales, pacientes_contagio, pacientes_loc, hospitales_loc
 
-def visualize_data():
+def visualize_data(pacientes_contagio, pacientes_loc, hospitales_loc):
     sombra_contagio = [int(sev*255/5) for sev in pacientes_contagio]
     color_contagio = ["#%02x0000" % (sev) for sev in sombra_contagio]
     tamano_contagio = [k**2.5 for k in pacientes_contagio]
@@ -79,7 +112,7 @@ def visualize_data():
     plt.axis('off')
     plt.show()
 
-def visualize_solution(plot_lineas):
+def visualize_solution(pacientes_contagio, pacientes_loc, hospitales_loc, plot_lineas):
     sombra_contagio = [int(sev*255/5) for sev in pacientes_contagio]
     color_contagio = ["#%02x0000" % (sev) for sev in sombra_contagio]
     tamano_contagio = [k**2.5 for k in pacientes_contagio]
@@ -94,13 +127,36 @@ def visualize_solution(plot_lineas):
     plt.axis('off')
     plt.show()
 
-def visualize_map():
+def visualize_map(pacientes_loc):
+    infectados = [[],[]]
+    for i in range(len(pacientes_loc)):
+        a,b = pacientes_loc[i]
+        infectados[0].append(a/100)
+        infectados[1].append(b/100)
     gmap = gmplot.GoogleMapPlotter(-12.1103929, -77.0347696, 14)
-    gmap.scatter( miraflores[0], miraflores[1], '#FF3333', size = 20, marker = False )
-    gmap.scatter( sanIsidro[0], sanIsidro[1], '#006633', size = 20, marker = False )
-    gmap.scatter( surquillo[0], surquillo[1], '#CC0066', size = 20, marker = False )
-    gmap.scatter( magdalena[0], magdalena[1], '#999900', size = 20, marker = False )
+    gmap.scatter( infectados[0], infectados[1], '#999900', size = 20, marker = False )
     gmap.scatter( hospitales[0], hospitales[1], '#000000', size = 100, marker = False )
     gmap.apikey = "AIzaSyA_2cjPy9AbF1aU1pa1oOo7_JmezBCy01c"
     gmap.draw( "map13.html" )
     webbrowser.open("map13.html",new=2)
+
+def visualiza_solumap(pacientes_loc, resultado):
+    infectados = [[],[]]
+    for i in range(len(pacientes_loc)):
+        a,b = pacientes_loc[i]
+        infectados[0].append(a/100)
+        infectados[1].append(b/100)
+    gmap = gmplot.GoogleMapPlotter(-12.1103929, -77.0347696, 14)    
+    gmap.scatter( hospitales[0], hospitales[1], '#000080', size = 100, marker = False )
+    gmap.scatter( infectados[0], infectados[1], '#999900', size = 20, marker = False )
+    colores = ['#FF0000','#808000','#FFA07A','#5B2C6F','#5367F4','#B8369F','#1A1362','#A04020','#117A65','#000000']
+    for i in range(10):
+        for (x_, y_) in resultado[i]:
+            x, y = zip(*[
+            (x_[0]/100,y_[0]/100),
+            (x_[1]/100,y_[1]/100),
+            ])
+            gmap.plot(x, y, colores[i], edge_width=1)
+    gmap.apikey = "AIzaSyA_2cjPy9AbF1aU1pa1oOo7_JmezBCy01c"
+    gmap.draw( "map14.html" )
+    webbrowser.open("map14.html",new=2)
--- code/gui_covid19.py
+++ code/gui_covid19.py
@@ -5,56 +5,68 @@ from CPmodel import *
 
 raiz = Tk() 
 raiz.title("COVID-19 PANDEMIA EN PERÚ") #Cambiar el nombre de la ventana 
-raiz.geometry("500x480") #Configurar tamaño
+raiz.geometry("500x500") #Configurar tamaño
 raiz.resizable(0,0)
 npaci = DoubleVar()
 time = DoubleVar()
+pl = 0
 
 fondo = PhotoImage(file="..\\imagenes\\fondo.gif")
 background_label = Label(image=fondo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 Label(raiz, text="Seleccione heuristica de variable: ",
-      width=25, anchor="sw").place(x=100, y=150)
+      width=25, anchor="sw").place(x=100, y=170)
 CHvar = ttk.Combobox(raiz, state="readonly", width=35,
                      values=["Primera variable",
                              "Variable con el valor mínimo más pequeño",
                              "Variable con el valor máximo más alto"])
 CHvar.current(0)
-CHvar.place(x=100, y=190)
+CHvar.place(x=100, y=210)
 
 Label(raiz, text="Seleccione heuristica de valor: ",
-      width=25, anchor="sw").place(x=100, y=240)
+      width=25, anchor="sw").place(x=100, y=260)
 CHval = ttk.Combobox(raiz, state="readonly", width=35,
                      values=["Menor valor", "Mayor valor",
                              "Valor medio inferior",
                              "Valor medio superior"])
 CHval.current(0)
-CHval.place(x=100, y=280)
+CHval.place(x=100, y=300)
+
+Label(raiz, text="Seleccione el caso a probar: ",
+      width=23, anchor="sw").place(x=100, y=30)
+Casos = ttk.Combobox(raiz, state="readonly", width=38,
+                     values=["Todos los pacientes, pero solo camas UCI",
+                             "401 pacientes, pero todas las camas"])
+Casos.current(0)
+Casos.place(x=100, y=70)
 
 def vdata():
-    visualize_data()
+    nh, pc, plo, hlo = generar_datos(Casos.current())
+    visualize_data(pc, plo, hlo)
 
 def vmap():
-    visualize_map()
+    nh, pc, plo, hlo = generar_datos(Casos.current())
+    visualize_map(plo)
 
 def vsolution():
-    pl,np,t = OrganizePandemic(n_camas_en_hospitales, pacientes_contagio,
-                                   pacientes_loc, hospitales_loc,
-                                   CHvar.current(), CHval.current())
+    nh, pc, plo, hlo = generar_datos(Casos.current())
+    pl,np,t = OrganizePandemic(nh, pc, plo, hlo,CHvar.current(), CHval.current())
     npaci.set(np)
     time.set(t)
-    if pl != 0: visualize_solution(pl)
+    if pl != 0:
+        visualiza_solumap(plo, pl)
+        visualize_solution(pc, plo, hlo, pl)
 
-Button(raiz, text="Ver datos del problema", command=vdata).place(x = 100, y=50)
-Button(raiz, text="Ver mapa con los datos", command=vmap).place(x = 100, y=100)
+Button(raiz, text="Ver datos del problema", command=vdata).place(x = 100, y=120)
+Button(raiz, text="Ver mapa con los datos", command=vmap).place(x = 270, y=120)
 Button(raiz, text="Ver solución del problema",
-       command=vsolution).place(x = 100, y=330)
+       command=vsolution).place(x = 100, y=350)
 Label(raiz, text="Numero de pacientes atendidos: ",
-      width=25, anchor="sw").place(x=100, y=380)
+      width=25, anchor="sw").place(x=100, y=400)
 Label(raiz, text="Tiempo de ejecución: ",
-      width=20, anchor="sw").place(x=100, y=410)
-ttk.Entry(raiz, textvariable=npaci, width=15).place(x=320, y=380)
-ttk.Entry(raiz, textvariable=time, width=15).place(x=320, y=410)
+      width=20, anchor="sw").place(x=100, y=430)
+ttk.Entry(raiz, textvariable=npaci, width=15).place(x=320, y=400)
+ttk.Entry(raiz, textvariable=time, width=15).place(x=320, y=430)
 
 raiz.mainloop()
